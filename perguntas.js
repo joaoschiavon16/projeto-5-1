12f49cb9886fc4f8c40f631d5b4a8b654d500f7b
joaoschiavon16 <00001107198495sp@al.educacao.sp.gov.br>
2024-12-02 14:14:55 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,26 @@
+criaCartao(
+    'Historia',
+    'Quem foi o responsável pela viagem que descobriu o Brasil em 1500?',
+    'Pedro Álvares Cabral.'
+
+
+)
+
+criaCartao(
+    'Historia',
+    'Qual foi o nome do tratado que dividiu o mundo entre Portugal e Espanha em 1494?',
+    'Tratado de Tordesilhas'
+)
+
+criaCartao(
+    'Historia',
+    'Quem foi o líder do movimento de independência da Índia contra o domínio britânico?',
+    'Mahatma Gandhi.'
+
+)
+
+criaCartao(
+    'Historia',
+    'O que foi a Revolução Industrial?',
+    'Foi um período de grandes mudanças tecnológicas e econômicas, marcado pela introdução de máquinas na produção, iniciado no final do século XVIII.'
+)
